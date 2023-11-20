b5e91e9cdfedc6d4d6e44dce7bdca035517857d7
noshpineapple28 <manoucherin@gmail.com>
2023-11-19 19:45:45 -0500

    fix: server port env

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:3000");
 });
 
 function log(msg) {
