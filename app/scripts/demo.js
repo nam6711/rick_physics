b2272cbb565437609bb621a4e9aeda690e84d059
noshpineapple28 <manoucherin@gmail.com>
2023-11-19 19:52:00 -0500

    fix: movie location

--- app/scripts/demo.js
+++ app/scripts/demo.js
@@ -2,7 +2,7 @@ let socket = io();
 
 let vid;
 function preload() {
-    vid = createVideo("http://localhost:3000/rick_roll");
+    vid = createVideo("https://people.rit.edu/nam6711/rick_roll.mp4");
 }
 
 function setup() {
